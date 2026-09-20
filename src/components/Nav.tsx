937e200a87ffd1c4fda99055cd77c3f705c6d085
Amirul Haziq <amirulhaziqshazlee@gmail.com>
2026-09-20 15:10:09 +0800

    Nav order: Roadmap before Blog

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,8 +12,8 @@ const LINKS: NavLink[] = [
   { href: '/#journey', label: 'Journey', id: 'journey', icon: JourneyIcon },
   { href: '/#work', label: 'Work', id: 'work', icon: WorkIcon },
   { href: '/#projects', label: 'Projects', id: 'projects', icon: ProjectsIcon },
-  { href: '/#blog', label: 'Blog', id: 'blog', icon: BlogIcon },
   { href: '/#roadmap', label: 'Roadmap', id: 'roadmap', icon: RoadmapIcon },
+  { href: '/#blog', label: 'Blog', id: 'blog', icon: BlogIcon },
   { href: '/#contact', label: 'Contact', id: 'contact', icon: ContactIcon },
 ];
 
